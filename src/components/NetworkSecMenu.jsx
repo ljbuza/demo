d3c43acfd89080f94a35c1225435a3ef083d69a9
Larry Buza <ljbuza@gmail.com>
2017-05-17 19:09:53 -0500

    changed background color

--- src/components/NetworkSecMenu.jsx
+++ src/components/NetworkSecMenu.jsx
@@ -1,64 +1,40 @@
-import React, { Component } from "react";
-import { Link, browserHistory } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { NavLink, Link, BrowserRouter } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
 export default class NetworkSecMenu extends Component {
-  state = { activeItem: "CMTS" };
-
-  handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name });
-    // browserHistory.push({ pathname: "/databrowser/network-mac" });
-  };
-
   render() {
-    const { activeItem } = this.state;
     // const section = this.props.section;
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item
-            name="CMTS"
-            as={Link}
-            to="/databrowser/network-cmts"
-            active={activeItem === "CMTS"}
-            onClick={this.handleItemClick}
-          />
+          <Menu.Item name="CMTS" as={NavLink} to="/databrowser/network-cmts" />
           <Menu.Item
             name="MAC domain"
-            as={Link}
+            as={NavLink}
             to="/databrowser/network-mac"
-            active={activeItem === "MAC domain"}
-            onClick={this.handleItemClick}
           />
           <Menu.Item
             name="fiber node"
-            as={Link}
+            as={NavLink}
             to="/databrowser/network-fiber"
-            active={activeItem === "fiber node"}
-            onClick={this.handleItemClick}
           />
           <Menu.Item
             name="CMTS interface"
-            as={Link}
+            as={NavLink}
             to="/databrowser/network-cface"
-            active={activeItem === "CMTS interface"}
-            onClick={this.handleItemClick}
           />
           <Menu.Item
             name="modem"
-            as={Link}
+            as={NavLink}
             to="/databrowser/network-modem"
-            active={activeItem === "modem"}
-            onClick={this.handleItemClick}
           />
           <Menu.Item
             name="modem interface"
-            as={Link}
+            as={NavLink}
             to="/databrowser/network-mface"
-            active={activeItem === "modem interface"}
-            onClick={this.handleItemClick}
           />
-          {/*<Menu.Menu position="right">
+          {/* <Menu.Menu position="right">
             <Menu.Item
               name="logout"
               active={activeItem === "logout"}
