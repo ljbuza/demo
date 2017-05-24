c0ef27b1abfce35f452241b2b829cf7e960fc4a8
Larry Buza <ljbuza@gmail.com>
2017-05-24 15:08:21 -0500

    removed stupidity

--- src/components/MapNetwork.jsx
+++ src/components/MapNetwork.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Image } from 'semantic-ui-react';
 // import Child from './Child.jsx';
 
-// export default class MapNetwork extends Component {
-//   handleChildClick(childData, event) {
-//     alert(
-//       'The Child button data is: ' +
-//         childData.childText +
-//         ' - ' +
-//         childData.childNumber,
-//     );
-//     alert('The Child HTML is: ' + event.target.outerHTML);
-//   }
+export default class MapNetwork extends Component {
+  //   handleChildClick(childData, event) {
+  //     alert(
+  //       'The Child button data is: ' +
+  //         childData.childText +
+  //         ' - ' +
+  //         childData.childNumber,
+  //     );
+  //     alert('The Child HTML is: ' + event.target.outerHTML);
+  //   }
   render() {
     return (
       <div>
